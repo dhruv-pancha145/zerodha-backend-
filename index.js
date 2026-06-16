ce15ef731cfc31f23e210e744e355baeca75400d
Dhruv Panchal <dhruvpanchal1345@gmail.com>
2026-06-16 18:02:59 +0530

    backend complete

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 3002;
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],  
+    origin: ["zerodha-frontend-lovat.vercel.app", "https://zerodha-dashboard-two-orpin.vercel.app"],  
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
